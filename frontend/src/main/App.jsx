1cdd56a6d3bf53d9001038cce200ea81097a2f95
eric-moniz <ericmoniz@outlook.com>
2023-07-03 20:55:50 -0300

    Aplicando Estilo css grid

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -5,12 +5,14 @@ import Footer from '../components/templates/Footer';
 import Logo from '../components/templates/Logo';
 import Main from '../components/templates/Main';
 import Nav from '../components/templates/Nav';
+import Header from '../components/templates/Header';
 
 const App = (props) => {
   return (
     <div className="app">
       <Logo />
       <Nav />
+      <Header />
       <Main />
       <Footer />
     </div>
