9557a864e8cf56a3390cc11ebe98f216eb60114e
eric-moniz <ericmoniz@outlook.com>
2023-07-03 21:24:32 -0300

    Componente Logo

--- frontend/src/components/templates/Logo.jsx
+++ frontend/src/components/templates/Logo.jsx
@@ -1,9 +1,15 @@
+import './Logo.css';
+import logo from '../../assets/img/logo.png';
 import React from 'react';
 
-import './Logo.css';
-
 const Logo = (props) => {
-  return <aside className="logo">Logo</aside>;
+  return (
+    <aside className="logo">
+      <a href="/" className="logo">
+        <img src={logo} alt="logo" />
+      </a>
+    </aside>
+  );
 };
 
 export default Logo;
