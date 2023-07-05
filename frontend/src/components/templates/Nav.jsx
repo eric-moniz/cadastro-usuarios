034cc24e047a55ac4ee576b39c64e16735385dbe
eric-moniz <ericmoniz@outlook.com>
2023-07-05 20:26:03 -0300

    Componente Navegação

--- frontend/src/components/templates/Nav.jsx
+++ frontend/src/components/templates/Nav.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 import './Nav.css';
 
 const Nav = (props) => {
-  return <aside className="menu-area">Navegação</aside>;
+  return (
+    <aside className="menu-area">
+      <nav className="menu">
+        {/* Refatorar */}
+        <a href="#/">
+          <i className="fa fa-home"></i> Início
+        </a>
+        <a href="#/users">
+          <i className="fa fa-users"></i> Usuários
+        </a>
+      </nav>
+    </aside>
+  );
 };
 
 export default Nav;
