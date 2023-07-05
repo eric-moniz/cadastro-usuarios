98d8cf8264b5c5faae9a4754becf62974800436d
eric-moniz <ericmoniz@outlook.com>
2023-07-05 19:40:16 -0300

    componente rodapé

--- frontend/src/components/templates/Footer.jsx
+++ frontend/src/components/templates/Footer.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = (props) => {
-  return <footer className="footer">Rodapé</footer>;
+  return (
+    <footer className="footer">
+      <span>
+        Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+        <strong>
+          {' '}
+          Cod<span className="text-danger">3</span>r
+        </strong>
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
